0a64244d626663b1111d1f02818e45d76d1d28de
unknown <curativeone@gmail.com>
2021-07-31 19:34:31 -0400

    Changed Earnings to Wages Test

---
+++ Server_Wage.py
@@ -0,0 +1,12 @@
+# Step 1: Input the Hours and Tips
+X = 5.44
+M = int(input("How many Hours Did you work?:"))
+B = int(input("What were your Tips?:"))
+
+#Step 2: Calculate the Average Wage for the Day
+Y = (M*X+B)
+Average = (Y/M)
+#Step 3: Display Results as a percentage.
+print ("Your Wages for the Day Are: ",format (Y, ',.2f'))
+print ("Your Average Wage for the Day Is: ",format (Average, ',.2f'))
+input()
